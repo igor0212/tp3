a6778256eb4250bfb1aa69d4e8ffd343ed32f5b9
Igor Oliveira Valente da Silveira <igor.silveira@dtidigital.com.br>
2019-11-16 18:01:25 -0300

    Lendo arquivo

--- src/utility/GraphUtilities.cpp
+++ src/utility/GraphUtilities.cpp
@@ -11,8 +11,7 @@ Graph GetGraphFromFile(string sudokuFile)
 {	
 	string line;	
 	ifstream inFile;
-	int N, M, I, counter=1;	
-	list<map<string, list<int>>> commands;
+	int N, I, J, counter=1;	
 	Graph graph;
 	
 	inFile.open("datasets/" + sudokuFile);	
@@ -28,38 +27,10 @@ Graph GetGraphFromFile(string sudokuFile)
 		istringstream s(line);		
 		if (counter == 1)
 		{  
-			//Armazenando primeira linha: numero de pessoas no time, numero de relações
-			// diretas entre os membros e numero de instrucoes e criando o grafo
-    		if (!(s >> N >> M >> I)) { break; }	
+			//Armazenando primeira linha: tamanho da tabela do Sudoku, quantidade de colunas
+			// e quantidade de linhas
+    		if (!(s >> N >> I >> J)) { break; }	
 			graph.Create(N);
-		} else if (counter == 2)
-		{	
-			//Criar dicionario (de/para) com as idades das pessoas
-			int age, index=0;			
-			while (s >> age) 
-			{
-				index++;
-			}	
-		}
-		else if (counter >= 3 && counter < (3 + M))
-		{
-			//Adicionar arestas no grafo
-			int commander, commanded;			
-			if (!(s >> commander >> commanded)) { break; }
-			graph.AddEdge(commander-1, commanded-1);
-		}
-		else if (counter >= (3 + M) && counter < ((3 + M) + I))
-		{
-			//Criar lista de dicionarios para os comandos
-			string command = ""; 
-			int commander = 0, commanded = 0;
-			list<int> listCommanderCommanded;
-			map<string, list<int>> cmdDic;			
-			s >> command >> commander >> commanded;			
-			if(commander != 0) listCommanderCommanded.push_back(commander-1);
-			if(commanded != 0) listCommanderCommanded.push_back(commanded-1);
-			cmdDic[command] = listCommanderCommanded;
-			commands.push_back(cmdDic);
 		}
 
 		counter++;
