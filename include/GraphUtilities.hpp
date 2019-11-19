243d57c2726df57cdb0192b779eb56f1f5048f7d
Igor Oliveira Valente da Silveira <igor.silveira@dtidigital.com.br>
2019-11-18 21:39:06 -0300

    Adicionando arestas linha e coluna

--- include/GraphUtilities.hpp
+++ include/GraphUtilities.hpp
@@ -3,8 +3,11 @@
 
 #include <Graph.hpp>
 #include <string>
+#include <vector>
 
 using namespace std;
+
+Graph AddEdge(Graph graph, vector<vector<int>> vertices, int N, int I, int J);
 
 Graph GetGraphFromFile(string sudokuFile);
 
--- src/utility/GraphUtilities.cpp
+++ src/utility/GraphUtilities.cpp
@@ -6,13 +6,34 @@
 
 using namespace std;
 
+Graph AddEdge(Graph graph, vector<vector<int>> vertices, int N, int I, int J)
+{
+    // Adicionando linhas ao sudoku
+    for(int i=0; i < N; i++)
+    {        
+        for(int j=0; j < N-1; j++)
+            cout << vertices[i][j] << " -> " << vertices[i][j+1] << "\n";
+        cout << "\n------------------\n";        
+    }
+
+    // Adicionando colunas ao sudoku
+    for(int i=0; i < N-1; i++)
+    {        
+        for(int j=0; j < N; j++)
+            cout << vertices[i][j] << " -> " << vertices[i+1][j] << "\n";
+        cout << "\n------------------\n";        
+    }
+
+    return graph;
+}
+
 Graph GetGraphFromFile(string sudokuFile)
 {	
 	string line;	
 	ifstream inFile;
 	int N, I, J, counter=1;	
 	Graph graph;
-    vector<int> vertices;
+    vector<vector<int>> vertices;
 	
 	inFile.open("datasets/" + sudokuFile);	
 
@@ -36,17 +57,22 @@ Graph GetGraphFromFile(string sudokuFile)
         {
             // Armazenando sudoku no vetor
             int verticeAux;
+            vector<int> verticesRow;
             for(int i = 0; i < N; i++)
             {
                 if (!(s >> verticeAux)) { break; }
-                vertices.push_back(verticeAux);
+                verticesRow.push_back(verticeAux);
             }
+
+            vertices.push_back(verticesRow);
         }
 
 		counter++;
 	}
 
 	inFile.close();	
+
+    graph = AddEdge(graph, vertices, N, I, J);    
 
 	return graph;
 }
@@ -81,7 +107,7 @@ void GreedyColoring(Graph graph)
                 available[result[*i]] = false; 
     } 
 
-    for (int u = 0; u < graph.V; u++) 
-        cout << "Vertex " << u << " --->  Color "
-             << result[u] << endl;	
+    // for (int u = 0; u < graph.V; u++) 
+    //     cout << "Vertex " << u << " --->  Color "
+    //          << result[u] << endl;	
 }
